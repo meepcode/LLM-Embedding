216cdb1f3353a499ac8be3ca57e020ff671e6ff7
AJ B <ajamesb13@gmail.com>
2025-04-17 15:22:37 -0400

    Add tokenize function

---
+++ functions.py
@@ -0,0 +1,12 @@
+import re
+
+# Tokenize
+def tokenize(text):
+    token_pattern = re.compile(r"\w+|[^\w\s]")
+    tokens = re.findall(token_pattern, text)
+    return tokens
+
+# Embed
+
+# Positional Embeddings
+# Feed into transformer
